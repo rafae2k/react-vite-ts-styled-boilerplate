844a06fb596564b60926dac3233309c28c932d4c
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-02-28 20:08:06 -0300

    feat: asd

---
+++ src/asd/index.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+import { Container } from "./styles";
+
+interface AsdProps {
+  children: ReactNode;
+}
---
+++ src/asd/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  color: #y3;
+`;
+
+export const ContainerStyle = styled.div``;
